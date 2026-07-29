d7ae9bda1e4cd28ec1548ecf3ab84bcc95c662fb
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-07-29 09:47:20 +0100

    refactor:redis client

--- helpers/redisClient.py
+++ helpers/redisClient.py
@@ -1,29 +1,26 @@
-import json
-from typing import TYPE_CHECKING
-
 from redis.asyncio import Redis
 
 redis_client = Redis(
     host="localhost",
     port=6379,
+    db=0,
     decode_responses=True,
 )
 
-channel = "capsule_logs"  # Redis channel for publishing capsule logs
-image_channel = "capsule_images"  # Redis channel for publishing capsule images
-if TYPE_CHECKING:
-    image_message: str
+STREAM = "capsule.jobs"
+GROUP = "article-workers"
+CONSUMER = "worker-1"
 
 
-class RedisClient:
-    async def queue_log(self, payload: dict):
-        await redis_client.lpush(
-            "capsule_processing",
-            json.dumps(payload),
-        )
+# class RedisClient:
+#     async def queue_log(self, payload: dict):
+#         await redis_client.lpush(
+#             "capsule_processing",
+#             json.dumps(payload),
+#         )
 
-    async def publish_log(self, message: str):
-        await redis_client.publish(
-            channel,
-            message,
-        )
+#     async def publish_log(self, message: str):
+#         await redis_client.publish(
+#             channel,
+#             message,
+#         )
