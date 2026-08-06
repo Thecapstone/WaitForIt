c776082024ea2ec1f49e95333e62797c06b1e2b4
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-08-06 06:23:25 +0100

    add:celery for batch aggregation

--- helpers/redisClient.py
+++ helpers/redisClient.py
@@ -1,3 +1,5 @@
+import json
+
 from redis.asyncio import Redis
 
 redis_client = Redis(
@@ -12,15 +14,16 @@ GROUP = "article-workers"
 CONSUMER = "worker-1"
 
 
-# class RedisClient:
-#     async def queue_log(self, payload: dict):
-#         await redis_client.lpush(
-#             "capsule_processing",
-#             json.dumps(payload),
-#         )
+# I would have liked to log out/print the contents of the raw_data
+# It would help me understand how to efficiently work wih the data,
+# If to access log description with bracket notation and push to a dictionary
+# then finally feed in the article as the log.
+# that would likely affect the structure of the article generation function.
+# Food For Thought
+# What is the likely output ot structure of the 'raw_data' content?
+def process_queued_logs(queue_connection, queue_name):
+    daily_logs = queue_connection.blpop(queue_name, timeout=10)
 
-#     async def publish_log(self, message: str):
-#         await redis_client.publish(
-#             channel,
-#             message,
-#         )
+    if daily_logs:
+        _queue_key, raw_data = daily_logs
+        json.loads(raw_data)
