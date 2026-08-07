ca3adecff41b853bc05361b5028fd7cebbe0b588
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-08-07 21:00:15 +0100

    fix:typo errors

--- config/celery.py
+++ config/celery.py
@@ -2,12 +2,12 @@ import os
 
 from celery import Celery
 
-os.envion.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.auto_discover_tasks()
+app.autodiscover_tasks()
 
 
 @app.task(bind=True, ignore_result=True)
