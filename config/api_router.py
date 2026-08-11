045efa99ae1803ff8f160085d981393a5170afca
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-08-11 02:59:01 +0100

    feat: waitlist urls

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,13 @@ from rest_framework import routers
 
 from authentication.views import AuthViewSet
 from memories.views import CapsuleViewSet
+from waitlist.views import WaitListViewSet
 
 app_name = "api"
 
 router = routers.SimpleRouter()
 router.register(r"memories", CapsuleViewSet, basename="memories")
 router.register(r"auth", AuthViewSet, basename="auth")
+router.register(r"waitlist", WaitListViewSet, basename="waitlist")
 
 urlpatterns = router.urls
