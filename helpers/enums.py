e21a0f77de455604b2b51816ff82d2287d05b24f
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-08-11 02:56:39 +0100

    feat: waitlist filter enum

--- helpers/enums.py
+++ helpers/enums.py
@@ -4,3 +4,8 @@ from django.db.models import TextChoices
 class UserPlan(TextChoices):
     Basic = {"basic": "basic"}
     Premium = {"premium": "premium"}
+
+
+class WaitListFilterKey(TextChoices):
+    ROLE = "role"
+    DATE = "date"
