853eb845a47868225abaa2a02fe1e3819a53ee29
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-08-16 19:15:56 +0100

    feat:waitlist routing

--- config/urls.py
+++ config/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
 urlpatterns += [
     # api_urls
     path("api/", include("config.api_router")),
+    path("api/v1/", include("waitlist.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
